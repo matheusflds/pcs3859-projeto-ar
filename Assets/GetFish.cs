1d0c3e44560ac88003e6a8210cce69cf17066e20
Luis Eduardo R Costa <luis.educ71@gmail.com>
2019-11-11 16:13:57 -0300

    Add back script

---
+++ Assets/GetFish.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetFish : MonoBehaviour
+{
+    public GameObject fishManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fishManager = GameObject.Find("FishManager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < fishManager.transform.childCount; i++)
+        {
+            GameObject child = fishManager.transform.GetChild(i).gameObject;
+            float distance = Vector3.Distance(child.transform.position, this.transform.position);
+            Debug.Log("Distance: " + distance);
+            if (distance <= 2f) {
+                Debug.Log("OI: " + child.ToString());
+            }
+        }
+    }
+}
